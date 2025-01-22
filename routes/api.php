5298e8fa5b7f9346642af39c4033dd93ff1b0652
Jan Michael Besinga <janmichaelbesinga873@gmail.com>
2025-01-22 14:55:20 +0800

    fix

--- routes/api.php
+++ routes/api.php
@@ -62,8 +62,13 @@ Route::group(['middleware'=>['auth:sanctum']],function(){
         Route::post('/attendance/{card_no}',[AttendanceController::class,'store']);
         Route::get('/get_yearly_attendances',[AttendanceController::class,'get_yearly_attendances']);
         Route::get('/top_gymmers',[AccountController::class,'get_top_gymmers']);
+<<<<<<< HEAD
 
         
+=======
+        Route::get('/get_top_gymmer_of_current_month',[AccountController::class,'get_top_gymmer_of_current_month']);
+        Route::get('/get_top_gymmer_of_current_month',[AccountController::class,'get_top_gymmer_of_current_month']);
+>>>>>>> 7df01b2 (stash)
         // SUMMARY
         Route::get('/get_attendance_summary',[SummaryController::class,'get_attendance_summary']); 
         Route::get('/get_sales_summary',[SummaryController::class,'get_sales_summary']);
@@ -75,6 +80,7 @@ Route::group(['middleware'=>['auth:sanctum']],function(){
 });
 // AUTHENTICATION
 Route::post('/auth/signin',[AuthController::class,'signin']);
+Route::get('/get_top_gymmer_of_current_month',[AccountController::class,'get_top_gymmer_of_current_month']);
 
 Route::get('/get_top_gymmer_of_current_month',[AccountController::class,'get_top_gymmer_of_current_month']);
 
